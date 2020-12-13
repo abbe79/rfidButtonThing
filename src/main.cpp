3ec1244448a1f8e8df534426c1ac0b446ac2265b
Tobias Abstreiter <tobias@abstreiter.de>
2020-12-13 23:40:21 +0100

    WifiManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <MFRC522.h>
+#include <MFRC522.h>     // https://github.com/miguelbalboa/rfid
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 #define RST_PIN 0
 #define SS_PIN  15
@@ -11,24 +12,35 @@ void setup() {
   Serial.begin(74880);
   while (!Serial);
 
+  Serial.println(F("Initialize WiFiManager"));
+  WiFiManager wifiManager;
+  wifiManager.autoConnect();
+
+	Serial.println(F("Initialize MFRC522"));
 	SPI.begin();
 	mfrc522.PCD_Init();
-	delay(40);
+	delay(5);
 	mfrc522.PCD_DumpVersionToSerial();
-	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+}
+
+String readCard(){
+  String result;
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    char buffer[3];
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      itoa(mfrc522.uid.uidByte[i], buffer, 16);
+      result += buffer;
+      if((i+1) < mfrc522.uid.size){
+        result += ':';
+      }
+    } 
+    Serial.print(F("Found card "));
+    Serial.println(result);
+  }
+  return result;
 }
 
 void loop() {
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  readCard();
+  delay(100);
 }
